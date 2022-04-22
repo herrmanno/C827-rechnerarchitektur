c076e3ac48b22a09cbed20b2516f9ea248a99b82
Oliver Herrmann <o.herrmann92@gmail.com>
2022-04-22 10:21:37 +0200

    Create build/* directories in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+mkdir -p build/{debug,release}
 cmake -B build/debug -S . -D CMAKE_BUILD_TYPE=Debug
 cmake -B build/release -S . -D CMAKE_BUILD_TYPE=Release
-cmake --build build/Debug
-cmake --build build/Release
+cmake --build build/debug
+cmake --build build/release
