eb9cc6f6f415e291f9f9c09a77c22c8eb82f7f3d
Oliver Herrmann <o.herrmann92@gmail.com>
2022-05-06 08:29:48 +0200

    Add loop unrolling

--- fib3.c
+++ fib3.c
@@ -1,20 +1,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static uint64_t fib_rec(uint32_t n, uint64_t a, uint64_t b) {
+#define LOOP (t = a + b, a = b, b = t)
+#define LOOP2 (LOOP, LOOP)
+#define LOOP4 (LOOP2, LOOP2)
+
+uint64_t fib(uint32_t n) {
+    uint64_t t = 0;
+    uint64_t a = 0;
+    uint64_t b = 1;
+
     start:
-    if (n == 1) {
+    if (n <= 1) {
         return a;
+    } else if (n > 4) {
+        LOOP4;
+        n -= 4;
+        goto start;
     } else {
+        LOOP;
         n -= 1;
-        uint64_t t = a + b;
-        a = b;
-        b = t;
         goto start;
     }
-}
-
-uint64_t fib(uint32_t size) {
-    return fib_rec(size, 0, 1);
 }
 
