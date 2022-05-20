136d9145e5c0307838e1cb9c032651039cf7b1ac
Oliver Herrmann <o.herrmann92@gmail.com>
2022-05-20 09:42:03 +0200

    Improve fib3.c
    - decrease instructions for loop; drop temp variable
    - add 'register' keywords

--- fib3.c
+++ fib3.c
@@ -1,14 +1,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define LOOP (t = a + b, a = b, b = t)
+#define LOOP (b = a + b, a = b - a)
 #define LOOP2 (LOOP, LOOP)
 #define LOOP4 (LOOP2, LOOP2)
 
 uint64_t fib(uint32_t n) {
-    uint64_t t = 0;
-    uint64_t a = 0;
-    uint64_t b = 1;
+    register uint64_t t = 0;
+    register uint64_t a = 0;
+    register uint64_t b = 1;
 
     start:
     if (n <= 1) {
