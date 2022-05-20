a0629c4fd2ca0832e0d2a787289221ea5a92db2d
Oliver Herrmann <o.herrmann92@gmail.com>
2022-05-20 09:36:51 +0200

    Add RISC compilation to build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,16 @@ cmake --build build/debug
 cmake --build build/release
 
 # Create objdump files
+echo "Creating objdump files"
 objdump -d --source build/debug/CMakeFiles/libfib3.dir/fib3.c.o > build/debug/fib3.objdump
 objdump -d --source build/release/CMakeFiles/libfib3.dir/fib3.c.o > build/release/fib3.objdump
+
+# Create risc files and objdumps
+if [ -f ~/risc-compiler/bin/riscv64-unknown-elf-gcc ]; then
+    echo "Compiling for RISC"
+    ~/risc-compiler/bin/riscv64-unknown-elf-gcc -O0 -g -c fib3.c -o build/debug/fib3.risc.o
+    ~/risc-compiler/bin/riscv64-unknown-elf-gcc -O3 -g -c fib3.c -o build/release/fib3.risc.o
+
+    ~/risc-compiler/bin/riscv64-unknown-elf-objdump -d --source build/debug/fib3.risc.o > build/debug/fib3.risc.objdump
+    ~/risc-compiler/bin/riscv64-unknown-elf-objdump -d --source build/release/fib3.risc.o > build/release/fib3.risc.objdump
+fi
