afa6824b2038235d7eecad28b7ebd5dd4d87a854
Oliver Herrmann <o.herrmann92@gmail.com>
2022-05-25 16:35:42 +0200

    Support parallel fib calculation

--- driver_multiple.c
+++ driver_multiple.c
@@ -7,15 +7,13 @@
 uint64_t fib(uint32_t);
 
 int main(int argc, char** argv) {
-    #pragma omp parallel for shared(argc,argv) private(end,n)
+    #pragma omp parallel for shared(argc,argv)
     for (uint32_t i = 1; i < argc; i++) {
-        char* end;
-        long n = strtoll(argv[i], &end, 10);
+        long n = strtoll(argv[i], 0, 10);
         if (errno != ERANGE && n != 0) {
             printf("%u: %llu\n", ((uint32_t) n), fib((uint32_t) n));
         } else {
-            printf("Bad argument");
-            return 1;
+            printf("Bad argument: %s", argv[i]);
         }
     }
 
