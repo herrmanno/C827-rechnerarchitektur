6389ac28c90873e4217db626bf22f4a07fa86dcf
Oliver Herrmann <herrmann@id-on.de>
2022-06-18 12:14:28 +0200

    Add 'build_remote' option

---
+++ build_remote.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+remote=${1:-htwk_imunix_perf}
+
+uuid=""
+if [ -f /proc/sys/kernel/random/uuid ]
+then
+    uuid=$(cat /proc/sys/kernel/random/uuid)
+elif [ -x $(which uuidgen) ]
+then
+    uuid=$(uuidgen)
+else
+    echo "Cannot create uuid (used for cloning repo on remote). Neither 'uuidgen' nor '/proc/sys/kernel/random/uuid' are available."
+    exit 1
+fi
+
+if [ -f ./build ]
+then
+    echo "Back up ./build to ./build~"
+    mv build build~
+fi
+
+echo "Building project at $remote:/tmp/$uuid"
+
+ssh -t "$remote" "pushd /tmp && git clone https://github.com/herrmanno/C827-rechnerarchitektur.git $uuid && pushd $uuid && bash build.sh"
+
+echo "Build succeeded"
+echo "Downloading artefacts"
+
+scp -r "$remote:/tmp/$uuid/build/*" "./build/"
+
+echo "Downloading artefacts succeeded"
+
+echo "Removing remote directory"
+
+ssh -t "$remote" "rm -rf /tmp/$uuid"
+
+echo "Remote directory removed"
