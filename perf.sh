91b2bd9ef282c7bf6e8cafa009d4fe55e1c437ce
Oliver Herrmann <herrmann@id-on.de>
2022-06-22 09:38:11 +0200

    Improve perf stat repeat count

--- perf.sh
+++ perf.sh
@@ -8,20 +8,20 @@ echo "Creating perf stat files"
 n=30
 for f in "${alg[@]}"
 do
-    perf stat -r 4 -o build/debug/fib_"$f"_"$n".stat build/debug/fib_$f $n
-    perf stat -r 4 -o build/release/fib_"$f"_"$n".stat build/release/fib_$f $n
+    perf stat -r 20 -o build/debug/fib_"$f"_"$n".stat build/debug/fib_$f $n
+    perf stat -r 20 -o build/release/fib_"$f"_"$n".stat build/release/fib_$f $n
 done
 
 n=10000
 for f in "${alg_wo_rec[@]}"
 do
-    perf stat -r 4 -o build/debug/fib_"$f"_"$n".stat build/debug/fib_$f $n
-    perf stat -r 4 -o build/release/fib_"$f"_"$n".stat build/release/fib_$f $n
+    perf stat -r 10 -o build/debug/fib_"$f"_"$n".stat build/debug/fib_$f $n
+    perf stat -r 10 -o build/release/fib_"$f"_"$n".stat build/release/fib_$f $n
 done
 
 n=1000000000
 for f in "${alg_wo_rec2[@]}"
 do
-    perf stat -r 4 -o build/debug/fib_"$f"_"$n".stat build/debug/fib_$f $n
-    perf stat -r 4 -o build/release/fib_"$f"_"$n".stat build/release/fib_$f $n
+    perf stat -r 5 -o build/debug/fib_"$f"_"$n".stat build/debug/fib_$f $n
+    perf stat -r 5 -o build/release/fib_"$f"_"$n".stat build/release/fib_$f $n
 done
